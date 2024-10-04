c75464d4b666a1931d96c0dd07ebdee9372cdec7
Cedrick Bienvenue <cedrick13bienvenue@gmail.com>
2024-10-04 14:56:11 +0200

    Added Background image

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import axios from "axios";
 import Dropdown from "./components/dropdown";
 import "./index.css";
 import Result from "./components/result";
-import bg from "./assets/bg.jpg";
 import { BiError } from "react-icons/bi";
 
 function App() {
@@ -31,7 +30,6 @@ function App() {
   };
 
   const handleConvert = async () => {
-
     if (!currency || isNaN(currency)) {
       setError(true);
       return;
@@ -59,7 +57,7 @@ function App() {
   return (
     <div
       style={{
-        backgroundImage: `url(${bg})`,
+        backgroundImage: `url("/bg.jpg")`,
         backgroundPosition: "center",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
@@ -67,7 +65,9 @@ function App() {
       className="font-poppins flex flex-col justify-center items-center h-[100vh] w-full px-4"
     >
       <div className="flex flex-col justify-center items-center mx-auto px-4 w-full md:w-[50%] md:px-[50px] py-[50px] backdrop-blur-[5px] bg-black bg-opacity-20 rounded-[10px]">
-        <h1 className="text-white font-semibold mb-[20px] md:mb-[30px] text-[20px] md:text-[40px]">Convert Currencies!</h1>
+        <h1 className="text-white font-semibold mb-[20px] md:mb-[30px] text-[20px] md:text-[40px]">
+          Convert Currencies!
+        </h1>
         <Input
           label={"Enter the money value"}
           onchange={(e) => {
@@ -86,26 +86,31 @@ function App() {
             key={2}
           />
         </div>
-        <button className="bg-[#ff227a] md:w-[300px] px-[40px] py-[10px] text-white rounded mt-10 hover:scale-105 transform transition" onClick={handleConvert}>Convert</button>
+        <button
+          className="bg-[#ff227a] md:w-[300px] px-[40px] py-[10px] text-white rounded mt-10 hover:scale-105 transform transition"
+          onClick={handleConvert}
+        >
+          Convert
+        </button>
         <Result result={result} />
       </div>
 
       {error && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 px-4 mx-auto">
-        <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col justify-center items-center text-center">
-        <BiError className="text-[60px] border-2 border-red-600 text-red-600 rounded-full p-[10px] mb-2" />
-          <h2 className="text-red-600 text-lg font-semibold">Error</h2>
-          <p className="text-gray-700 mt-2">
-            Please enter a valid number to convert as currency.
-          </p>
-          <button
-            className="bg-red-500 text-white px-4 py-2 rounded mt-4 hover:bg-red-600 transition"
-            onClick={closeModal}
-          >
-            Close
-          </button>
+          <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col justify-center items-center text-center">
+            <BiError className="text-[60px] border-2 border-red-600 text-red-600 rounded-full p-[10px] mb-2" />
+            <h2 className="text-red-600 text-lg font-semibold">Error</h2>
+            <p className="text-gray-700 mt-2">
+              Please enter a valid number to convert as currency.
+            </p>
+            <button
+              className="bg-red-500 text-white px-4 py-2 rounded mt-4 hover:bg-red-600 transition"
+              onClick={closeModal}
+            >
+              Close
+            </button>
+          </div>
         </div>
-      </div>
       )}
     </div>
   );
